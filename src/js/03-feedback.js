1850753050b64068d978e294742a885215bc00d1
Domisiaxa <domisiaxa@gmail.com>
2022-12-30 01:37:42 +0100

    x

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,59 +1,29 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input[type="email"]'),
-  message: document.querySelector('textarea[name="message"]'),
-};
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
 
-refs.form.addEventListener('input', throttle(onInput, 500));
-refs.form.addEventListener('submit', onFormSubmit);
+const formData = {};
 
-const STORAGE_INPUT_KEY = 'feedback-form-state';
-
-function onInput(e) {
-  const userDetails = JSON.parse(localStorage.getItem(STORAGE_INPUT_KEY)) || {};
-  console.log(userDetails);
-  userDetails[e.target.name] = e.target.value;
-
-  localStorage.setItem(STORAGE_INPUT_KEY, JSON.stringify(userDetails));
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-const saveLocalItems = localStorage.getItem(STORAGE_INPUT_KEY);
-const parsSaveLocalItems = JSON.parse(saveLocalItems);
-
-function getLocalStorageItems() {
-  if (saveLocalItems) {
-    !parsSaveLocalItems.email
-      ? ''
-      : (refs.email.value = parsSaveLocalItems.email);
-    !parsSaveLocalItems.message
-      ? ''
-      : (refs.message.value = parsSaveLocalItems.message);
-  }
-}
-
-getLocalStorageItems();
-
-function onFormSubmit(e) {
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   e.preventDefault();
-
-  const {
-    elements: { email, message },
-  } = e.currentTarget;
-
-  if (email.value === '' || message.value === '') {
-    return alert('Введіть всі поля форми!!!');
-  }
-
-  const formElDetails = { email: email.value, message: message.value };
-  console.log(formElDetails);
-
   e.currentTarget.reset();
-
-  removeStorageItems();
+  localStorage.removeItem('feedback-form-state');
 }
 
-function removeStorageItems() {
-  localStorage.removeItem(STORAGE_INPUT_KEY);
-}
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
